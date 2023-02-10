fb7dd3c6731ea6476d474c6c5006577330ceee8a
Archibasov2023 <arch1405@yandex.ru>
2023-02-10 23:48:23 +0400

    add Решение домашнего задания "Код на C#"

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿// Console.WriteLine("Input ferst number");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input second number");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// if (num1 > num2)
+// {
+//     Console.WriteLine("ferst number is max");
+// }
+// else
+// {
+//     Console.WriteLine("second number is max");
+// }
+// Задача2.
+
+
+
+// Console.WriteLine("Input ferst number");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input second number");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input third number");
+// int num3 = Convert.ToInt32(Console.ReadLine());
+
+// if (num1 > num2)
+//     {
+//         if (num1 > num3)
+//         {
+//             Console.WriteLine("ferst number is max");
+//         }
+//         else
+//         {
+//             Console.WriteLine("third number is max");
+//         }
+//     }
+//     else
+//     {
+//         if (num2 > num3)
+//         {
+//             Console.WriteLine("second number is max");
+//         }
+//         else
+//         {
+//             Console.WriteLine("third number is max");
+//         }
+//     }
+// Задача 4.
+
+
+
+// Console.WriteLine("Input number");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// if (num % 2 == 0)
+// {
+//     Console.WriteLine("Number is even");
+// }
+// else
+// {
+//     Console.WriteLine("Number is odd");
+// }
+// Задача 6.
+
+
+
+// Console.WriteLine("Input number");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int current = 2;
+
+// while (current <= num - 1)
+// {
+//     Console.Write(current + " ");
+//     current = current + 2;
+// }
+// Задача 8.
